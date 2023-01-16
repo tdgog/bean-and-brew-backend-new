dffdc84e324766a64f7ad350f3ad4e98b0c98b33
tdgog <65415295+tdgog@users.noreply.github.com>
2023-01-16 16:25:39 +0000

    daf

--- index.js
+++ index.js
@@ -22,42 +22,77 @@ const userModel = mongoose.model('user', new mongoose.Schema({
     }
 }))
 
+async function generateToken(userId) {
+    let futureDate = new Date();
+    futureDate.setDate(futureDate.getDate() + 14);
+    const sessionToken = userId + futureDate.getMilliseconds();
+
+    const hashedToken = await new Promise((resolve, reject) => {
+        bcrypt.genSalt(10, function(err, tokenSalt) {
+            bcrypt.hash(sessionToken, tokenSalt, function (err, hashedToken) {
+                resolve(hashedToken);
+            })
+        })
+    })
+
+    return [hashedToken, futureDate]
+}
+
+router.post('/login', jsonParser, async (req, res) => {
+    const { email, hash } = req.body;
+    console.log(email, hash);
+
+    if(await isUnique(email)) return;
+
+    userModel.findOne({ email: email, hash: hash }, async function(err, user) {
+        console.log(err, user);
+        const userId = user.get('_id');
+        const [hashedToken, futureDate] = await generateToken(userId);
+        await userModel.findByIdAndUpdate(userId, { token: { token: hashedToken, expiry: futureDate } });
+
+        res.json({ success: true, token: hashedToken, expiry: futureDate });
+    });
+    res.json({ success: false });
+})
+
+function isUnique(email) {
+    return new Promise((resolve, reject) => {
+        userModel.countDocuments({ email: email }, function(err, count) {
+            resolve(count === 0);
+        });
+    })
+}
+
 router.post('/isUnique', jsonParser, async (req, res) => {
     const { email } = req.body;
-    userModel.countDocuments({ email: email }, function(err, count) {
-        res.json({ unique: count === 0 });
-    });
+    res.json({ unique: await isUnique(email) });
 })
 
 router.post('/addUser', jsonParser, async (req, res) => {
     const { email, hash } = req.body;
 
-    // Create session token
+    if(!(await isUnique(email))) return;
+
     const userId = new mongoose.Types.ObjectId();
-    let futureDate = new Date();
-    futureDate.setDate(futureDate.getDate() + 14);
-    const sessionToken = userId + futureDate.getMilliseconds();
-    bcrypt.genSalt(10, function(err, tokenSalt) {
-        bcrypt.hash(sessionToken, tokenSalt, function (err, hashedToken) {
-            const user = new userModel({
-                _id: userId,
-                email: email,
-                hash: hash,
-                token: {
-                    token: hashedToken,
-                    expiry: futureDate
-                }
-            })
-            user.save()
-                .then(result => {
-                    res.json({ success: true, token: hashedToken, expiry: futureDate });
-                })
-                .catch(error => {
-                    console.error(error);
-                    res.json({ success: false });
-                })
+    const [hashedToken, futureDate] = await generateToken(userId);
+    const user = new userModel({
+        _id: userId,
+        email: email,
+        hash: hash,
+        token: {
+            token: hashedToken,
+            expiry: futureDate
+        }
+    })
+    user.save()
+        .then(result => {
+            console.log('Created account.')
+            res.json({ success: true, token: hashedToken, expiry: futureDate });
         })
-    });
+        .catch(error => {
+            console.error(error);
+            res.json({ success: false });
+        })
 })
 
 app.use(bodyParser.json())
